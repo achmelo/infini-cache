a150a081dc5705bbf24ec62d86670f1577c5007e
achmelo <a.chmelo@gmail.com>
2021-10-18 17:18:05 +0200

    persistent storage

--- src/main/java/sk/cache/kes/InfiniConfig.java
+++ src/main/java/sk/cache/kes/InfiniConfig.java
@@ -3,8 +3,6 @@ package sk.cache.kes;
 import org.infinispan.commons.api.CacheContainerAdmin;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
-import org.infinispan.configuration.global.GlobalConfiguration;
-import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +17,10 @@ public class InfiniConfig {
 
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.clustering().cacheMode(CacheMode.DIST_SYNC);
+        builder.persistence().passivation(true)
+                .addSoftIndexFileStore()
+                .shared(false)
+                .dataLocation("data").indexLocation("index");
         cacheManager.administration()
                 .withFlags(CacheContainerAdmin.AdminFlag.VOLATILE)
                 .getOrCreateCache("myCache", builder.build());
