b8f7eca29cb0cf36916c41bc126ac467418b4178
achmelo <a.chmelo@gmail.com>
2021-10-20 10:33:22 +0200

    read initial hosts from env variable

--- src/main/java/sk/cache/kes/InfiniConfig.java
+++ src/main/java/sk/cache/kes/InfiniConfig.java
@@ -1,19 +1,32 @@
 package sk.cache.kes;
 
 import org.infinispan.commons.api.CacheContainerAdmin;
+import org.infinispan.commons.dataconversion.MediaType;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ParserRegistry;
 import org.infinispan.manager.DefaultCacheManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class InfiniConfig {
+    @Value("${jgroups.tcpping.initial_hosts}")
+    private String initialHosts;
     @Bean
     DefaultCacheManager cacheManager() throws IOException {
-        DefaultCacheManager cacheManager = new DefaultCacheManager("infinispan.xml");
+        System.setProperty("jgroups.tcpping.initial_hosts", initialHosts);
+
+        InputStream configurationStream = new FileInputStream("./src/main/resources/infinispan.xml");
+        ConfigurationBuilderHolder holder = new ParserRegistry().parse(configurationStream, null, MediaType.APPLICATION_XML);
+
+        DefaultCacheManager cacheManager = new DefaultCacheManager(holder, true);
 
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.clustering().cacheMode(CacheMode.DIST_SYNC);
